96ebc5d769df34ff1d3ed212ccb5c1de066fcb67
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-09-21 02:53:12 +0300

    feat: added menu-burger

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("160 из 150, все пункты выполнены")
+
+document.querySelector('.burger').addEventListener('click', function(){
+    document.querySelector('.burger span').classList.toggle('active');
+    document.querySelector('.menu').classList.toggle("animate");
+  })
+
+  
+  
